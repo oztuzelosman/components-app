53655c774ca8af1da5c06b1839e75764637747bc
oztuzelosman <oztuzelosman@gmail.com>
2023-03-17 08:35:55 +0000

    Update App.js and Button.js

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ import { GiClover } from "react-icons/gi";
 function App() {
   return (
     <div>
-      <Button primary>
+      <Button primary rounded outline>
         <GiClover />
         Hide
       </Button>
-      <Button secondary>
+      <Button secondary rounded outline>
         <GiClover />
         Buy
       </Button>
-      <Button succes>
+      <Button success rounded outline>
         <GiClover />
         Cancel
       </Button>
-      <Button warning>
+      <Button warning rounded outline>
         <GiClover />
         Skip
       </Button>
-      <Button danger>
+      <Button danger rounded outline>
         <GiClover />
         Add to Cart
       </Button>
