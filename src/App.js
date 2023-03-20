d088f98ba350aa86f7741cd6703dde085fd8d2cf
oztuzelosman <oztuzelosman@gmail.com>
2023-03-20 22:49:41 +0300

    add button and event handler to accordion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import { useState } from "react";
 import dataItems from "./Datas";
+//
+import { RiArrowDownSFill } from "react-icons/ri";
+//
 function App() {
-  const [expandedIndex, setExpandedIndex] = useState(2);
+  const [expandedIndex, setExpandedIndex] = useState(false);
+
+  const getButtonId = (buttonId) => {
+    console.log(buttonId);
+    setExpandedIndex(buttonId - 1);
+  };
 
   const renderedItems = dataItems.map((dataItem, index) => {
     const isExpanded = index === expandedIndex;
 
     return (
-      <div className="m-5 p-3 border border-red-400 rounded-xl">
-        <h3 className="text-gray-500">{dataItem.header}</h3>
+      <div
+        key={dataItem.id}
+        className="m-5 p-3 border-2 border-gray-400 rounded-xl"
+      >
+        <div className="flex flex-row">
+          <h3 className="flex-1 text-gray-500">{dataItem.header}</h3>
+          <button
+            onClick={() => {
+              getButtonId(dataItem.id);
+            }}
+            className=" text-3xl"
+          >
+            <RiArrowDownSFill />
+          </button>
+        </div>
         {isExpanded && <p>{dataItem.content}</p>}
       </div>
     );
