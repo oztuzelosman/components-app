b38d59cedb6f94143c209c015bc976af21d8cdbd
oztuzelosman <oztuzelosman@gmail.com>
2023-03-20 11:06:49 +0000

    tweaking with styles

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ import { GiClover } from "react-icons/gi";
 function App() {
   return (
     <div>
-      <Button primary rounded outline>
+      <Button primary rounded>
         <GiClover />
         Hide
       </Button>
-      <Button secondary rounded outline>
+      <Button secondary rounded>
         <GiClover />
         Buy
       </Button>
-      <Button success rounded outline>
+      <Button success rounded>
         <GiClover />
         Cancel
       </Button>
-      <Button warning rounded outline>
+      <Button warning rounded>
         <GiClover />
         Skip
       </Button>
-      <Button danger rounded outline>
+      <Button danger rounded>
         <GiClover />
         Add to Cart
       </Button>
