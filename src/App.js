ce43d3e61d1ba1e01651e3f1289826b6a01181f7
oztuzelosman <oztuzelosman@gmail.com>
2023-03-20 22:04:32 +0300

    start trying accordion func with event handlers

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import { useState } from "react";
-
+import dataItems from "./Datas";
 function App() {
-  const dataItems = [
-    {
-      id: 1,
-      header: "I love Ramazan Abi!",
-      content: "It increases my developing skills so much",
-    },
-    {
-      id: 2,
-      header: "I love Css!",
-      content: "It increases my developing skills so much",
-    },
-    {
-      id: 3,
-      header: "I love Js!",
-      content: "It increases my developing skills so much",
-    },
-  ];
-
-  const [expandedIndex, setExpandedIndex] = useState(1);
+  const [expandedIndex, setExpandedIndex] = useState(2);
 
   const renderedItems = dataItems.map((dataItem, index) => {
     const isExpanded = index === expandedIndex;
 
     return (
       <div className="m-5 p-3 border border-red-400 rounded-xl">
-        <span>{dataItem.id}</span>
-        <h3 className="text-lg">{dataItem.header}</h3>
+        <h3 className="text-gray-500">{dataItem.header}</h3>
         {isExpanded && <p>{dataItem.content}</p>}
       </div>
     );
---
+++ src/Datas.js
@@ -0,0 +1,19 @@
+const dataItems = [
+  {
+    id: 1,
+    header: "I love Ramazan Abi!",
+    content: "It increases my developing skills so much",
+  },
+  {
+    id: 2,
+    header: "I love Css!",
+    content: "It increases my developing skills so much",
+  },
+  {
+    id: 3,
+    header: "I love Js!",
+    content: "It increases my developing skills so much",
+  },
+];
+
+export default dataItems;
